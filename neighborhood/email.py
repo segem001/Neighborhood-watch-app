d72177b5a4aa8e6b3d781d82aacea082e0556f00
segem001 <kipdom001@gmail.com>
2022-03-24 20:09:27 +0300

    modify the email.py file

---
+++ neighborhood/email.py
@@ -0,0 +1,14 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+def send_welcome_email(name,receiver):
+    # Creating message subject and sender
+    subject = 'Welcome to the MoringaTribune NewsLetter'
+    sender = 'kipdom001@gmail.com'
+
+    #passing in the context variables
+    text_content = render_to_string('email/newsmail.txt',{"name": name})
+    html_content = render_to_string('email/newsmail.html',{"name":name})
+    msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
+    msg.attach_alternative(html_content,'text/html')
+    msg.send()
